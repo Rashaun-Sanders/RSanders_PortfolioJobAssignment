ac829efb671821d2c589ddb43d9d1c76a15f0eb5
ArkhamRider468 <W209458400@student.hccs.edu>
2021-02-18 00:40:05 -0600

    Begin Player Creation

---
+++ unity-gb-master/Assets/First Person Character/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Transform playerCamera = null;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMouseLook();
+    }
+
+    void UpdateMouseLook()
+    {
+        Vector2 mouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        transform.Rotate(Vector3.up * mouseDelta.x);
+
+    }
+}
