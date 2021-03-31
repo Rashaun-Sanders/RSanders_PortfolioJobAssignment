6437919530d3f175d0da6ece13ae5600c34de4ee
ArkhamRider468 <W209458400@student.hccs.edu>
2021-03-30 22:57:36 -0500

    Create and Leave Room
    Create and Leave room are now functional.

--- unity-gb-master/Assets/Programming Job Assignemnts/5 - Multiplayer/Launcher.cs
+++ unity-gb-master/Assets/Programming Job Assignemnts/5 - Multiplayer/Launcher.cs
@@ -6,6 +6,8 @@ using TMPro;
 public class Launcher : MonoBehaviourPunCallbacks
 {
     [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] TMP_Text errorText;
+    [SerializeField] TMP_Text roomNameText;
 
     // Start is called before the first frame update
     void Start()
@@ -34,16 +36,29 @@ public class Launcher : MonoBehaviourPunCallbacks
             return;
         }
         PhotonNetwork.CreateRoom(roomNameInputField.text);
-        MenuManager.Instance.OpenMenu("loading");
+        MenuManager.Instance.OpenMenu("Loading");
     }
 
     public override void OnJoinedRoom()
     {
-        
+        MenuManager.Instance.OpenMenu("Room");
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
-        
+        errorText.text = " Room Creation Failed: " + message;
+        MenuManager.Instance.OpenMenu("Error");
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.Instance.OpenMenu("Loading");
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.Instance.OpenMenu("Title");
     }
 }
